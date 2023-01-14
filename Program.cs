ff9121206f7e088f91f5df57bae428069a109279
amansoni <amansoni2041999@gmail.com>
2023-01-14 23:25:44 +0530

    performing employeewage

--- Program.cs
+++ Program.cs
@@ -8,18 +8,20 @@ namespace EmployeeWage222
 {
     internal class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
+
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmployeeWage Computation Program");
-            int empHrs;
+            //variables
+            int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
+            //computation
             while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
@@ -30,18 +32,22 @@ namespace EmployeeWage222
                     case IS_FULL_TIME:
                         empHrs = 8;
                         break;
+
                     case IS_PART_TIME:
                         empHrs = 4;
                         break;
+
                     default:
                         empHrs = 0;
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + "  " + "Working Hours : " + empHrs);
+                Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine($"totalEmpHrs : {totalEmpHrs}  *  EMP_RATE_PER_HOUR : {EMP_RATE_PER_HOUR} = totalEmpWage : {totalEmpWage}");
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+
+
             Console.ReadLine();
 
 
